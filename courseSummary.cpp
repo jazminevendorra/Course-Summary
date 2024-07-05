907e77928df3a6ed983b1b387127f9ed7b69d470
Jazmin <163955648+jazminevendorra@users.noreply.github.com>
2024-07-05 11:00:53 -0400

    Update courseSummary.cpp

--- courseSummary.cpp
+++ courseSummary.cpp
@@ -1,8 +1,7 @@
 // This program will show a menu-driven interface that provides the user with two options:
 // 1. Process Grades Summary report.
 // 2. Quit.
-// 
-// Jazmin Even Dorra
+
 
 
 //Header Files
